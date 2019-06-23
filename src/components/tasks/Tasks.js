81ee241ac7701fc47621d64e9cafd279ffabf661
José Rodolfo <j.rodolfopps@gmail.com>
2019-06-23 12:42:16 -0300

    Adicionado a listagem no nosso container de tasks. O mesmo se encontra mostrando uma listagem Fake

--- src/components/tasks/Tasks.js
+++ src/components/tasks/Tasks.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
+import List from "./list/List";
 
 export default class Tasks extends Component {
   render() {
@@ -7,9 +8,11 @@ export default class Tasks extends Component {
       <Row>
         <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
           <p className="title">To-do</p>
+          <List tasks={ [ {title: 'Criar Header Criar Header Criar Header', done: false}, {title: 'Criar footer', done: false}, {title: 'Criar footer', done: false}] } />
         </Col>
         <Col xs={{ span: 8, offset: 2 }} className="tasks_list">
           <p className="title">Done</p>
+          <List tasks={ [ {title: 'Criar Header Criar Header Criar Header', done: true}, {title: 'Criar footer', done: true}, {title: 'Criar footer', done: true}] } />
         </Col>
       </Row>
     );
---
+++ src/components/tasks/list/List.js
@@ -0,0 +1,39 @@
+import React, { Component, Fragment } from "react";
+import { Card, Table, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default class List extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Card>
+          <Card.Body>
+            <Table responsive>
+              <tbody>
+                {this.props.tasks.map((task, index) => {
+                  return <tr key={task.id}>
+                    <td className="col-md-10">{task.title}</td>
+                    <td>
+                      {
+                        task.done == false ?
+                          <a className="check" href="#">
+                            <FontAwesomeIcon icon="check-circle" />
+                          </a>
+                        : null
+                      }
+                    </td>
+                    <td>
+                      <a className="delete" href="#">
+                        <FontAwesomeIcon icon="trash-alt" />
+                      </a>
+                    </td>
+                  </tr>;
+                })}
+              </tbody>
+            </Table>
+          </Card.Body>
+        </Card>
+      </Fragment>
+    );
+  }
+}
