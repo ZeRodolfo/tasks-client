5370309272af7b3e5f3d983aeb3ec7de928dece0
José Rodolfo <j.rodolfopps@gmail.com>
2019-06-24 11:15:51 -0300

    Criado o metodo que irá atualizar a lista de tarefas pendentes para concluída

--- src/components/tasks/list/List.js
+++ src/components/tasks/list/List.js
@@ -1,15 +1,28 @@
 import React, { Component, Fragment } from "react";
 import { Card, Table, Row } from "react-bootstrap";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import api from '../../../api.js'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import api from "../../../api.js";
 
 export default class List extends Component {
-  
+  async checkTask(task) {
+    const form = {
+      task: {
+        done: true
+      }
+    };
+
+    await api.put(`tasks/${task.id}`, form).then(res => {
+      this.props.loadTasks();
+    });
+  }
+
   async deleteTask(task) {
-    if (window.confirm(`Você realmente deseja excluir o item: ${task.title}?`)) {
+    if (
+      window.confirm(`Você realmente deseja excluir o item: ${task.title}?`)
+    ) {
       await api.delete(`tasks/${task.id}`).then(res => {
         this.props.loadTasks();
-      })
+      });
     }
   }
 
@@ -21,23 +34,31 @@ export default class List extends Component {
             <Table responsive>
               <tbody>
                 {this.props.tasks.map((task, index) => {
-                  return <tr key={task.id}>
-                    <td className="col-md-10">{task.title}</td>
-                    <td>
-                      {
-                        task.done === false ?
-                          <a className="check" href="#">
+                  return (
+                    <tr key={task.id}>
+                      <td className="col-md-10">{task.title}</td>
+                      <td>
+                        {task.done === false ? (
+                          <a
+                            className="check"
+                            href="#"
+                            onClick={() => this.checkTask(task)}
+                          >
                             <FontAwesomeIcon icon="check-circle" />
                           </a>
-                        : null
-                      }
-                    </td>
-                    <td>
-                      <a className="delete" href="#" onClick={() => this.deleteTask(task)}>
-                        <FontAwesomeIcon icon="trash-alt" />
-                      </a>
-                    </td>
-                  </tr>;
+                        ) : null}
+                      </td>
+                      <td>
+                        <a
+                          className="delete"
+                          href="#"
+                          onClick={() => this.deleteTask(task)}
+                        >
+                          <FontAwesomeIcon icon="trash-alt" />
+                        </a>
+                      </td>
+                    </tr>
+                  );
                 })}
               </tbody>
             </Table>
